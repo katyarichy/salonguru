0709106895b6a5684ab007babbf87d49505e1e5d
Kateryna Marchenko <katya.richy@gmail.com>
2024-09-26 16:46:24 +0200

    basic setup

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiUrl:
+    'https://g93902zutc.execute-api.eu-central-1.amazonaws.com/prod/products',
+  checkoutUrl:
+    'https://g93902zutc.execute-api.eu-central-1.amazonaws.com/prod/checkout',
+  apiKey: '2XyCVKIz277ZMEirPNTyQ3814iBXY5rg9ddTfX4W',
+};
